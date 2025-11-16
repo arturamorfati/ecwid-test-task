14e978be22a9e42a4dca564f52a8e7b20ced0028
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-16 15:21:10 +0500

    fix: index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,15 +3,17 @@ import ProductCard from "~/components/ProductCard.vue";
 import CategoryCard from "~/components/CategoryCard.vue";
 import { useProducts } from "~/api/useProducts";
 import { useCategories } from "~/api/useCategories";
+import { computed } from "vue";
 
 const { data: productsData } = useProducts();
 const { data: categoriesData } = useCategories();
 
-const products = productsData?.value?.items || [];
-const categories = categoriesData?.value?.items || [];
+const products = computed(() => productsData?.value?.items || []);
+const categories = computed(() => categoriesData?.value?.items || []);
 </script>
 
 <template>
+
   <div class="container mx-auto p-4">
     <h1 class="text-2xl font-bold mb-4">Categories</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-8">
