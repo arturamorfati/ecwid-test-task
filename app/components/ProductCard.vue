8fc6757438ddc3608aefeeaa8182ea9c68fb25a7
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-16 14:30:41 +0500

    feat: add ProductCard

---
+++ app/components/ProductCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { Product } from "~/api/types";
+import { defineProps } from "vue";
+import { useCartStore } from "~/stores/cart";
+
+const props = defineProps<{ product: Product }>();
+const cartStore = useCartStore();
+
+const buy = () => cartStore.add(props.product);
+</script>
+
+<template>
+  <Card class="p-4">
+    <img
+      :src="product.thumbnailUrl"
+      class="w-full h-48 object-cover rounded-md"
+    />
+    <h3 class="text-lg font-semibold mt-2">{{ product.name }}</h3>
+    <p class="text-gray-600 mt-1">€ {{ product.price.toFixed(2) }}</p>
+    <Button
+      class="mt-2 w-full"
+      @click="buy"
+      >Buy</Button
+    >
+  </Card>
+</template>
