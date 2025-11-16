e7f48f226c6995619a3ceaee00eca68b6e7ac182
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-16 15:14:30 +0500

    feat: render NuxtPage

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-import Button from "./components/ui/button/Button.vue";
-import { useCategories } from "./api/useCategories";
-import { useProducts } from "./api/useProducts";
-import { useProduct } from "./api/useProduct";
-
-const { data: categories } = useCategories();
-const { data: products } = useProducts();
-const { data: product } = useProduct(1);
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
-  <div>
-    <Button>button</Button>
-  </div>
+  <NuxtPage />
 </template>
