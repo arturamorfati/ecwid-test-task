1585f06d24caba9166bc55f1dfcc30ba2701a768
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-16 14:28:02 +0500

    feat: add CartIcon

---
+++ app/components/CartIcon.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useCartStore } from "~/stores/cart";
+import { useRouter } from "vue-router";
+import { computed } from "vue";
+
+const cartStore = useCartStore();
+const router = useRouter();
+
+const count = computed(() => cartStore.count);
+
+const goToCart = () => router.push("/cart");
+</script>
+
+<template>
+  <Button
+    variant="secondary"
+    @click="goToCart"
+  >
+    Cart ({{ count }})
+  </Button>
+</template>
