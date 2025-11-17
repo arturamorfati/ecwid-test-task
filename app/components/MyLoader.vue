e03d5d66f41c3a86f1953aced40a49f5520f54a3
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-17 19:25:36 +0500

    feat: add Loader

---
+++ app/components/MyLoader.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full flex justify-center py-20">
+    <div class="animate-spin rounded-full h-12 w-12 border-4 border-gray-300 border-t-transparent" />
+  </div>
+</template>
