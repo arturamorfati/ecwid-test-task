c908307cc4cace34e7582282402fb92aa65b5039
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-17 19:25:59 +0500

    feat: update ProductCard

--- app/components/ProductCard.vue
+++ app/components/ProductCard.vue
@@ -12,24 +12,27 @@ const buy = () => cartStore.add(props.product);
 </script>
 
 <template>
-  <Card class="p-4">
-    <img
-      :src="product.thumbnailUrl"
-      class="w-full h-48 object-cover rounded-md"
-    />
-    <h3 class="text-lg font-semibold mt-2">{{ product.name }}</h3>
+  <NuxtLink :href="`/products/${product.id}`">
+    <Card class="p-4 h-full">
+      <img
+        :src="product.thumbnailUrl"
+        class="w-full h-48 object-cover rounded-md"
+        :alt="`${product.name} thumbnail`"
+      />
+      <h3 class="text-lg font-semibold mt-2">{{ product.name }}</h3>
 
-    <div class="h-2" />
+      <div class="h-2" />
 
-    <div class="mt-auto w-full flex justify-between items-center">
-      <p>Price:</p>
-      <p class="text-gray-600 mt-1">€ {{ product.price.toFixed(2) }}</p>
-    </div>
+      <div class="mt-auto w-full flex justify-between items-center">
+        <p>Price:</p>
+        <p class="text-gray-600 mt-1">€ {{ product.price.toFixed(2) }}</p>
+      </div>
 
-    <Button
-      class="w-full"
-      @click="buy"
-      >Buy</Button
-    >
-  </Card>
+      <Button
+        class="w-full"
+        @click="buy"
+        >Buy</Button
+      >
+    </Card>
+  </NuxtLink>
 </template>
