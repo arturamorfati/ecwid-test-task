3aa7359db17856c0ccbd6146c23dab712d4eacf0
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-17 18:43:56 +0500

    feat: update product page

--- app/pages/products/[id].vue
+++ app/pages/products/[id].vue
@@ -2,29 +2,45 @@
 import { useRoute } from "vue-router";
 import { useProduct } from "~/api/useProduct";
 import { useCartStore } from "~/stores/cart";
+import Button from "~/components/ui/button/Button.vue";
+import MyLoader from "~/components/MyLoader.vue";
 
 const route = useRoute();
 const productId = Number(route.params.id);
 const { data: productData } = useProduct(productId);
 
-const product = productData.value!;
+const product = productData.value;
 const cartStore = useCartStore();
 
-const buy = () => cartStore.add(product);
+const buy = () => {
+  if (!product) return;
+
+  cartStore.add(product);
+};
 </script>
 
 <template>
-  <div class="container mx-auto p-4">
+  <MyLoader v-if="!product" />
+
+  <div
+    v-else
+    class="container mx-auto p-4"
+  >
     <img
-      :src="product.imageUrl"
-      class="w-full h-96 object-cover rounded-md mb-4"
+      :src="product?.imageUrl"
+      class="w-full h-96 object-contain rounded-md mb-4"
     />
     <h1 class="text-3xl font-bold mb-2">{{ product.name }}</h1>
     <p class="text-gray-600 mb-2">€ {{ product.price.toFixed(2) }}</p>
-    <div
+    <p
       v-html="product.description"
       class="mb-4"
-    ></div>
-    <Button @click="buy">Buy</Button>
+    ></p>
+    <Button
+      class="w-full"
+      :disabled="!product"
+      @click="buy"
+      >Buy</Button
+    >
   </div>
 </template>
