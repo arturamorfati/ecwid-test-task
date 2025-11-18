fd7b18fb09762c4205a4e5f2df0b838a451ca6e8
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-18 12:25:51 +0500

    feat: remove router logic form CartIcon

--- app/components/CartIcon.vue
+++ app/components/CartIcon.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
 import { useCartStore } from "~/stores/cart";
-import { useRouter } from "vue-router";
 import { computed } from "vue";
 import Button from "./ui/button/Button.vue";
 
 const cartStore = useCartStore();
-const router = useRouter();
 
 const count = computed(() => (cartStore?.count ? cartStore.count : 0));
 
-const goToCart = () => router.push("/cart");
 </script>
 
 <template>
   <Button
     variant="secondary"
-    @click="goToCart"
   >
     Cart ({{ count }})
   </Button>
