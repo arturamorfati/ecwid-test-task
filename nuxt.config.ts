2251b4d8913217192faae74d7c7eeccbcc663ef4
Artur Sadrtdinov <asadrtdinov2003@gmail.com>
2025-11-18 12:43:58 +0500

    feat: add gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  app: {
+    baseURL: "/ecwid-test-task/",
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"],
+    },
+  },
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/image",
-    "@nuxtjs/tailwindcss",
-    "shadcn-nuxt",
-    "@vueuse/nuxt",
-    "@pinia/nuxt",
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt", "@vueuse/nuxt", "@pinia/nuxt"],
   shadcn: {
     componentDir: "@/components/ui",
   },
